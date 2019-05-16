14d34f96554d69dc5824b2719350e96600bef4ff
Nina Kazzim <kazzim@icloud.com>
2019-05-16 19:09:22 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0 
-    puts "Fizz"
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  elsif int % 3 == 0 
+    return "Fizz"
   elsif int % 5 == 0
-   puts "Buzz"
-  elsif int % 5 && int % 3 == 0
-   puts "FizzBuzz"
- else
+   return "Buzz"
+  else
    return nil
  end
 end
